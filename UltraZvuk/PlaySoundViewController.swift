04e8ac918b6067791b605abe9ae687358a8c0d54
Petr Stenin <stenpety@petrs-mbp.wimaxion>
2017-02-11 19:44:46 +0400

    All pictures uploaded, buttons and constrains set

---
+++ UltraZvuk/PlaySoundViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PlaySoundViewController.swift
+//  UltraZvuk
+//
+//  Created by Petr Stenin on 11/02/2017.
+//  Copyright © 2017 Petr Stenin. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
